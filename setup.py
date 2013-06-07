00a7ad95bfc31b7086edbaae4f39f3cb48aa2ee7
Joel Hermanns <joel.hermanns@gmail.com>
2013-06-07 20:27:05 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-elm",
+    packages = find_packages(),
+    version = "0.1",
+    author = "Joel Hermanns",
+    author_email = "joel.hermanns@gmail.com",
+    url = "",
+    description = "Django template tags to compile Elm to Javascript",
+    classifiers = [
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords = ["elm"],
+)
