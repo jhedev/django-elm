9263010f03823ea8e2a3ba57c65a3240a347d5b4
Joel Hermanns <joel.hermanns@gmail.com>
2013-06-07 21:45:27 +0200

    Added static files in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="django-elm",
     packages = find_packages(),
-    package_data={'': ['bin/elmToJS.hs']},
+    package_data={'': ['bin/elmToJS.hs'],'': ['static/js/*']},
     version = "0.1",
     author = "Joel Hermanns",
     author_email = "joel.hermanns@gmail.com",
